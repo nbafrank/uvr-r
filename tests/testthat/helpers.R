c1beb7667157113f5bd40d7558810c74d9e7fc88
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-01 12:38:35 -0700

    remove skip_if_no_uvr()

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,11 +1,3 @@
-skip_if_no_uvr <- function() {
-  candidate_path <- file.path(.get_home_dir(), ".cargo", "bin", "uvr")
-  skip_if_not(
-    nzchar(Sys.which("uvr")) || file.exists(candidate_path),
-    message = "uvr binary not found"
-  )
-}
-
 setup_uvr_test <- function() {
   path <- try(find_uvr())
   if (!nzchar(path)) {

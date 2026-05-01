10fdfb13294fb35a3f99ef8cbee4ac9519e1dd3a
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-01 11:24:01 -0700

    add test that run_uvr works for a basic command

--- tests/testthat/test-run_uvr.R
+++ tests/testthat/test-run_uvr.R
@@ -1,4 +1,5 @@
-test_that("run_uvr errors on bad command", {
+test_that("run_uvr works and errors on bad command", {
   path <- setup_uvr_test() # installs uvr if needed
+  expect_no_error(run_uvr(args = c("r", "list"), bin = path))
   expect_error(run_uvr(args = "--nonexistent-flag", bin = path), "exited with code")
 })

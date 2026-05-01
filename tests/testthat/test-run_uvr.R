30e2df2a8e1d1e78d947f23f79c47ae2723c76bd
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-01 10:19:13 -0700

    try installing uvr if not found when testing run_uvr

--- tests/testthat/test-run_uvr.R
+++ tests/testthat/test-run_uvr.R
@@ -1,4 +1,8 @@
 test_that("run_uvr errors on bad command", {
-  skip_if_no_uvr()
+  # Try installing if not found 
+  path <- find_uvr()
+  if (!file.exists(path)) {
+    install_uvr()
+  }
   expect_error(run_uvr("--nonexistent-flag"), "exited with code")
 })

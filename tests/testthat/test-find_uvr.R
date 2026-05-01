a565fd2fcca5cf59fe57efa5b9a30d9d99869737
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-01 10:16:18 -0700

    try installing uvr in find_uvr test if not initially found

--- tests/testthat/test-find_uvr.R
+++ tests/testthat/test-find_uvr.R
@@ -1,5 +1,10 @@
 test_that("find_uvr returns a path when uvr is installed", {
-  skip_if_no_uvr()
+  path <- find_uvr()
+
+  # Try installing if not found 
+  if (!file.exists(path)) {
+    install_uvr()
+  }
   path <- find_uvr()
   expect_true(file.exists(path))
 })

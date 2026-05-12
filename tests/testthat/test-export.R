6477b598e5cca30a3bc6d7a824f9d17abca14795
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-12 08:22:09 -0700

    comment out ren.lock file creation test until issue #83 resolved

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -6,8 +6,9 @@ test_that("export works and can output to any file name", {
   init(bin = path, dir = temp_dir)
   add("jsonlite", bin = path, dir = temp_dir)
 
-  expect_no_error(export(out_file = "renv.lock", bin = path, dir = temp_dir))
-  expect_true(file.exists(file.path(temp_dir, "renv.lock")))
+  # TODO: enable after issue #83 resolved
+  # expect_no_error(export(out_file = "renv.lock", bin = path, dir = temp_dir))
+  # expect_true(file.exists(file.path(temp_dir, "renv.lock")))
 
   expect_no_error(export(
     out_file = "renv-test.locktest",

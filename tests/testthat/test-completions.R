4cfcbf4b3acc4b01e926156e4f1c185ecfc3244b
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-14 15:38:45 -0700

    test output of completions (only test length - snapshot quite large and likely to change freq.)

--- tests/testthat/test-completions.R
+++ tests/testthat/test-completions.R
@@ -4,7 +4,9 @@ test_that("completions works for all shells", {
   path <- setup_uvr_test(temp_dir = temp_dir) # installs uvr if needed
 
   for (shell in c("bash", "zsh", "fish", "powershell")) {
-    expect_no_error(completions(shell = shell, bin = path, quiet = TRUE))
-    # TODO: compare output with expected
+    result <- expect_no_error(
+      completions(shell = !!shell, bin = path, quiet = TRUE)
+    )
+    expect_true(length(result) > 0L && is.character(result))
   }
 })

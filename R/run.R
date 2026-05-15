9fa4b35e50bcf8eac799a282871743e327dca93f
Brayden Nilson <nilson.brayden@gmail.com>
2026-05-15 09:07:03 -0700

    implement extra_libs arg in run() to temp. set UVR_EXTRA_LIBS

--- R/run.R
+++ R/run.R
@@ -6,6 +6,9 @@
 #'
 #' @param script Path to an R script.
 #' @param args Character vector of arguments forwarded to the script. Defaults to \code{NULL} for no arguments.
+#' @param extra_libs Character vector of paths to additional library directories to be included.
+#'   Temporarily sets the UVR_EXTRA_LIBS environment variable.
+#'   Defaults to \code{NULL} for no extra libraries.
 #' @inheritParams run_uvr
 #' @inherit run_uvr return
 #' @export
@@ -17,14 +20,24 @@
 run <- function(
   script,
   args = NULL,
+  extra_libs = NULL,
   bin = NULL,
   dir = NULL,
   quiet = FALSE
 ) {
   .validate_single_characters(list(script = script))
-  .validate_multi_characters(list(args = args), null_ok = TRUE)
+  .validate_multi_characters(
+    list(args = args, extra_libs = extra_libs),
+    null_ok = TRUE
+  )
   .validate_single_characters(list(bin = bin, dir = dir), null_ok = TRUE)
   .validate_flags(list(quiet = quiet))
+
+  if (!is.null(extra_libs)) {
+    sep <- ifelse(.Platform$OS.type == "unix", ":", ";")
+    extra_libs <- paste(extra_libs, collapse = sep)
+  }
+  .temp_setenv(list(UVR_EXTRA_LIBS = extra_libs))
 
   cli_args <- c("run", script)
   if (length(args) > 0L) {
